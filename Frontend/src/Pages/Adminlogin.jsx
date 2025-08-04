ac278960763b84886386bf79e08212cc15ac2250
Jastej Singh <pvsjas4609@gmail.com>
2025-08-04 18:46:02 +0530

    Fix import case for CSS files in Adminlogin, Cart, and ProductDetail components

--- Frontend/src/Pages/Adminlogin.jsx
+++ Frontend/src/Pages/Adminlogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Register.css';
+import './Adminlogin.css';
 import { useNavigate,Link } from 'react-router-dom';
 const login = () => {
   
